4625e391d230197f4ce9416b78639bf937c710b2
Richard <221830172@smail.nju.edu.cn>
2024-04-02 19:50:31 +0800

    111

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ import org.antlr.v4.runtime.Vocabulary;
 import java.io.IOException;
 import java.util.List;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length < 1) {
             System.err.println("input path is required");
@@ -24,47 +23,33 @@ public class Main
 
         Vocabulary vocabulary = sysYLexer.getVocabulary();
 
-        if(ErrorListener.isError == true){
+        if (ErrorListener.isError == true) {
             ErrorListener.printLexerErrorInformation();
-        }
-        else {
-            for(Token t : myTokens){
-                String text = t.getText();
-                int line = t.getLine();
-                int type = t.getType();
-                String sym = vocabulary.getSymbolicName(type);
-                if(sym.equals("INTEGR_CONST")){
-                    if(text.charAt(0) == '0' && text.length() >= 2){
-                        if(text.startsWith("0x") || text.startsWith("0X")){
-                            text = String.valueOf(Long.parseLong(text.substring(2),16));
-                        }
-                        else {
-                            text = String.valueOf(Long.parseLong(text.substring(1),8));
-                        }
-                    }
-                }
-                System.err.println(sym + " " + text + " at Line " +line +".");
+        } else {
+            for (Token t : myTokens) {
+                printSysYTokenInformation(t, vocabulary);
             }
         }
 
 
-//[token类型] [token文本] at Line [此token首个字符所在行的行号].
-        //MINUS - at Line 3.
-
-
-//        if (myErrorListener has ErrorInformation) {
-//            // 假设myErrorListener有一个错误信息输出函数printLexerErrorInformation.
-//            myErrorListener.printLexerErrorInformation();
-//        } else {
-//            for (Token t : myTokens) {
-//                printSysYTokenInformation(t);
-//            }
-//        }
-
-
     }
 
-
+    public static void printSysYTokenInformation(Token t, Vocabulary vocabulary) {
+        String text = t.getText();
+        int line = t.getLine();
+        int type = t.getType();
+        String sym = vocabulary.getSymbolicName(type);
+        if (sym.equals("INTEGR_CONST")) {
+            if (text.charAt(0) == '0' && text.length() >= 2) {
+                if (text.startsWith("0x") || text.startsWith("0X")) {
+                    text = String.valueOf(Long.parseLong(text.substring(2), 16));
+                } else {
+                    text = String.valueOf(Long.parseLong(text.substring(1), 8));
+                }
+            }
+        }
+        System.err.println(sym + " " + text + " at Line " + line + ".");
+    }
 
 
 }
