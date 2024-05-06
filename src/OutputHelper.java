6b54bf1c58e362f848c17301b620dc3b09f1b832
Richard <221830172@smail.nju.edu.cn>
2024-05-06 16:51:12 +0800

    111

--- src/OutputHelper.java
+++ src/OutputHelper.java
@@ -3,29 +3,30 @@ import java.util.*;
 public class OutputHelper {
 
     // 创建一个映射，将索引映射到对应的值
-    static Set<Index> set = new HashSet<>();
+    static Map<Integer,Integer> map = new HashMap<>();
     public static boolean isFalse = false;
 
     public static void printSemanticError(ErrorType errorType, int line, String text) {
         isFalse = true;
-        if(errorType.getType() == 6){
-            if(!checkCut(6,line)){
-                System.err.println("Error type " + errorType.getType() + " at Line " + line + ":[" + text + ":" + errorType + "].");
-            }
-        }
-        else{
+
+        if(checkCut(line,errorType.getType())){
             System.err.println("Error type " + errorType.getType() + " at Line " + line + ":[" + text + ":" + errorType + "].");
         }
+
+
 
         //Error type [errorTypeNo] at Line [lineNo]:[errorMessage]
     }
 
-    private static boolean checkCut(int value, int line) {
-        Index index = new Index(value,line);
-        if (set.contains(index)) {
+    private static boolean checkCut(int line, int value) {
+        if(!map.containsKey(line)){
+
+            map.put(line,value);
             return true;
         }
-        set.add(index);
+        if(map.get(line) == value){
+            return true;
+        }
         return false;
     }
 }
